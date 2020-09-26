a13a987966d01d285c7b236607830502170b3f1d
venkey <venkeyvenkatesh885@gmail.com>
2020-09-26 07:45:15 +0530

    [venkey] intital commit

--- first_project_wage/Program.cs
+++ first_project_wage/Program.cs
@@ -7,6 +7,18 @@ namespace first_project_wage
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
         }
     }
 }
