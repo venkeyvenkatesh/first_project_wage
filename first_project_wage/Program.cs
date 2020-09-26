27570e0d6f75cfaa4da43943f7d730f92ee89265
venkey <venkeyvenkatesh885@gmail.com>
2020-09-26 07:38:38 +0530

    [venkey] intital commit

---
+++ first_project_wage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace first_project_wage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
